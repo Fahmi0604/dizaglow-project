71b14c05b86da99ef0b1210c5679364fc633e994
fahmi0604 <fahmifirmansyah917@gmail.com>
2022-11-27 20:56:16 +0700

    update assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
         "primary": '#F0A500',
         "secondary": '#FF6881',
         "custom-black": '#1B1A17',
-        "custom-muted": '#87898B'
+        "custom-muted": '#87898B',
+        "custom-red": '#BA3232'
       },
       fontFamily: {
         'Proxima': ['Proxima Nova', 'sans-serif'],
